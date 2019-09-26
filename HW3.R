dfc6c3d232238145e51aa4f9b8eac6f9d1fbedc8
mkin411 <marguerite.kinsella1@louisiana.edu>
2019-09-25 19:34:58 -0500

    updated

--- HW3.R
+++ HW3.R
@@ -11,5 +11,21 @@ d8<-read.csv("Aurelia_")
 
 
 
-#loadingIIS
-#aver
+#loading IIS201405281105.text 
+s1 <- read_csv(file = "ISIIS201405281105.txt")
+s1
+s2<-read_csv(file = "ISIIS201405281609.txt")
+s2
+s3<-read_csv(file = "ISIIS201405281124.txt")
+s3
+s4<-read_csv(file = "ISIIS201405281215.txt")
+s4
+
+#Aureliafiles
+library(readxl)
+s5<-read_xlsx("Aurelia_SEAMAP_2012-2018_30minCell.xlsx")
+s5
+s6<-load("aurelia_15minCell_statareas.Rdata")
+s6
+head(a)
+
