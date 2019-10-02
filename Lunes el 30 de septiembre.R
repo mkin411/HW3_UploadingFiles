e319f0335b9a7c1a61c65cdb762184e9527f836e
mkin411 <marguerite.kinsella1@louisiana.edu>
2019-10-02 15:45:53 -0500

    october 1 data

--- Lunes el 30 de septiembre.R
+++ Lunes el 30 de septiembre.R
@@ -85,3 +85,84 @@ library(nutshell)
 data("batting.2008")
 d <- batting.2008
 head(d)
+#tapply -- (tidyverse function)
+#how do you want to summarize your data?
+str(d)
+#number of home runs per team, HR
+hr<-tapply(X = d$HR, INDEX = list(d$teamID), FUN = sum)
+#needs to be uppercase X
+hr
+#interested in variance
+## find quantile values for home runs
+#five num gives you min, lower hinge, median, upper-hinge, and max value
+fhr<-tapply(X = d$HR, INDEX = list(d$teamID), FUN = fivenum)
+fhr
+#lower hinde and upper hinge 17th and 20th percentiles
+lq.q<-tapply(X = d$H/d$AB, INDEX = list(d$lgID), FUN = fivenum)
+lq.q
+#proportion of hits for at bat
+head(d$lgID)
+summary(d[d$lgID== "AL",] $H/d[d$lgID=="AL",]$AB)
+#review al functions
+#summarize over multiple categories
+bats<-tapply(X= d$HR, INDEX = list(d$lgID, d$bats), FUN = mean)
+unique(d$bats)
+bats.team<-tapply(X= d$HR, INDEX = list(d$lgID, d$teamID), FUN = mean)
+bats.team
+#three categories
+bats.team2<-tapply(X= d$HR, INDEX = list(d$lgID, d$teamID,d$bats), FUN = mean)
+bats.team2
+#aggregate ---- 
+team.stats.sum<-aggregate(x=d[,c("AB", "H", "BB", "2B", "HR")], by=list(d$teamID), FUN=sum)
+#tidyverse summarise
+#passes the output for whatevers on the left to to the function group
+#group by assigns groups by indicators you want to give
+team.sum = d %>% group_by(teamID) %>% summarise(ABsum = sum(AB), ABmean= mean(AB), Absd= sd(AB), ABcount= n())
+head(team.sum)
+#AB count is number of observations
+#summarise, can do them all at the same time versus aggregate, summarise is more flexible
+team.sum2 = d %>% group_by(lgID, teamID) %>% summarise(ABsum = sum(AB), ABmean= mean(AB), Absd= sd(AB), ABcount= n())
+team.sum2
+#league, team and your data, added another field 
+#aggregatevs summarise, use of summarise more intuitive
+#rowsum ----
+#when you just want to add up the values in each row
+rs<-rowsum(d[c("AB", "H", "HR", "2B", "3B")],group =d$teamID)
+head(d[c("AB", "H", "HR", "2B", "3B")])
+#use the function tabulate
+HR.cnts<-tabulate(d$HR)
+names(HR.cnts) <-0:length(HR.cnts-1)
+#sequence, 0 to the number of valuesl started at 0 then subtracted it, assigned it each value for each team
+#number of times that number , one player hit 47 hr
+#name function do not need to be a vector
+HR.cnts
+#table ----
+table(d$bats)
+table(d[,c("bats", "throws")])
+#giving us different categories
+length(d$team)
+#aside about the name function ----
+m<- matrix(nrow=4, ncol=3)
+m
+colnames(m)<-c("one", "two", "three")
+rownames(m)<-c("apple", "pear", "orange", "berry")
+#reshaping your data ----
+n<-matrix(1:10, nrow=5)
+n
+t(n)
+#cols have become the rows
+v <-1:10
+t(v)
+str(t(v))
+#reshape data, using stack and unstack ----
+names(d)
+s<-d[,c("lgID", "teamID", "AB", "HR", "throws")]
+s.un<-unstack(x =s, form = teamID ~ AB)
+s.un
+head(s.un)
+#melt and cast
+library(reshape2)
+#cast function to change data frame from the long to the wide format
+s.wide<-dcast(data= s, value.var = "HR",formula = "lgID" ~"teamID" ,fun.aggregate = mean)
+str(s.wide)
+
